9c24d1db97ba39e22e92db52c75cb33e5f48c5bc
Obsidiwayv <pizza5803@gmail.com>
2025-06-28 22:52:34 -0400

    first commit

---
+++ Lexer.cs
@@ -0,0 +1,35 @@
+﻿namespace Wayvlyte.BladeLang;
+
+public class Lexer
+{
+    public List<BladeNode> nodes = [];
+
+    public List<List<string>> keywords = [
+        ["proj", Keywords.Project]
+    ];
+
+    public void Parse(string fileContent)
+    {
+        var words = fileContent.Trim().Split(" ");
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            var word = words[i];
+
+            if (word.StartsWith('{') && word != "{")
+            {
+                nodes.Add(new BladeNode(Tokens.StartLoop, word));
+            }
+            else if (word.EndsWith('}') && word != "}")
+            {
+                nodes.Add(new BladeNode(Tokens.EndLoop, word));
+            }
+
+            // Check if there is another word and go ahead
+            if (i + 1 < words.Length && )
+            {
+
+            }
+        }
+    }
+}
---
+++ Tokens.cs
@@ -0,0 +1,28 @@
+namespace Wayvlyte.BladeLang;
+
+public enum Tokens
+{
+    Keyword,
+    Value,
+    EndLoop,
+    StartLoop
+}
+
+// Keyword enums to be assigned to keywords
+public enum Keywords
+{
+    Project,
+    Libaries,
+    Runtimes,
+    Includes,
+    UnrealNaming,
+    Assets
+}
+
+// A tree class object to be used for parsing
+public class BladeNode(Tokens token, string value)
+{
+    public Tokens Token { get; } = token;
+
+    public string Value { get; } = value;
+}
